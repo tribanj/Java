f7239aff0c22f654b77ba69140c7c37deaf4f7ae
Tribhuwan <tribhuwanja@gmail.com>
2023-07-19 20:41:37 +0530

    Setup is compleated

--- index.java
+++ index.java
@@ -1,7 +1,6 @@
 class index {
-    public static void main(String[] args) {
-
-        System.out.println("Hello tribhuwan");
-    }
+   public static void main(String[] args) {
+    System.out.println("Hello teibhuwan nice work");
+   }
 
 }
