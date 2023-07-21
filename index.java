427d03245937c32a37263fd139883d3e8c6bbdc7
Tribhuwan <tribhuwanja@gmail.com>
2023-07-21 21:55:26 +0530

    for loop done

--- index.java
+++ index.java
@@ -1,17 +1,37 @@
 import java.util.Scanner;
 
 public class index {
-   public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
-      int n = sc.nextInt();
-      int sum = 0;
-      int num = 1;
+   // public static void main(String[] args) {
+   // Scanner sc = new Scanner(System.in);
+   // int n = sc.nextInt();
+   // int sum = 0;
+   // int num = 1;
 
-      while (num <= n) {
-         // System.out.println(num);
-         sum = sum + num;
-         num++; // increment operator
-      }
-      System.out.println(sum);
-   }
+   // while (num <= n) {
+   // // System.out.println(num);
+   // sum = sum + num;
+   // num++; // increment operator
+   // }
+   // System.out.println(sum);
+   // }
+
+
+   //for loop 
+   // public static void main(String[] args) {
+   //    Scanner sc = new Scanner(System.in);
+   //    int n = sc.nextInt();
+   //    int sum = 0;
+   //    int num = 1;
+   //    for (num = 1; num <= n; num++) {
+   //       sum = sum + num;
+
+   //    }
+   //    System.out.println(sum);
+
+   // }
+
+
+
+   // Do while Loop 
+
 }
