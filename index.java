97fcff4125026d2eef70431ce5e22994ad69e00a
Tribhuwan <tribhuwanja@gmail.com>
2023-07-21 21:21:59 +0530

    while loop

--- index.java
+++ index.java
@@ -1,6 +1,16 @@
-class index {
-   public static void main(String[] args) {
-    System.out.println("Hello teibhuwan nice work");
-   }
+import java.util.Scanner;
 
+public class index {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int num = 1;
+         
+
+        while(num <= n){
+            System.out.println(num);
+            num ++ ;  //increment operator
+        }
+    }
 }
---
+++ loops/loop.java
@@ -0,0 +1,16 @@
+// import java.util.Scanner;
+
+// public class loops {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+        
+//         int num = 1;
+         
+
+//         while(num <= n){
+//             System.out.println(num);
+//             num = num++ ;  //increment operator
+//         }
+//     }
+// }
