7e60de92cb16d00561b2478d6b95286c5352adba
Tribhuwan <tribhuwanja@gmail.com>
2023-08-03 20:31:48 +0530

    sum of series and factorial is completed

---
+++ loopsProblems/sumOfSeries.java
@@ -0,0 +1,23 @@
+// series is S = 1-2+3-4......n
+
+import java.util.Scanner;
+
+public class sumOfSeries {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+
+        int ans = 0;
+        for(int i = 1; i <= n; i++){
+            if(i % 2 == 0){
+                ans -= i;
+
+            }else{
+                ans += i;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
