39bd007b773e19b4f6dce59d12961e76bb84d4dc
Tribhuwan <tribhuwanja@gmail.com>
2023-08-03 19:48:07 +0530

    sum of digits

---
+++ loopsProblems/CountDigits.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class CountDigits {
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     int n = sc.nextInt();
+    //     int numOfDigits = 0;
+    //     int original_n = n;
+
+    //     while (n > 0) {
+    //         n = n / 10;
+    //         numOfDigits++;
+    //     }
+
+    //     System.out.println("Number of Digits in " + original_n + " = " + numOfDigits);
+    // }
+
+
+
+// Question num 2:-/
+// find the sum of digits in a given number 
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int SumOfDigits = 0;
+    int original_n = n;
+
+
+    while(n > 0){
+        SumOfDigits += n % 10;
+        n= n/10;
+    }
+    System.out.println("Number of Digits in " + original_n + " = " + SumOfDigits);
+    
+}
+
+
+
+}
