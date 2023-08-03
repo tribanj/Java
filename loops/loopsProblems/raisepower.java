56e0a8ecea5650d8a99bb0a38864bb0d55c597bb
Tribhuwan <tribhuwanja@gmail.com>
2023-08-03 20:41:15 +0530

    power calculation is done

---
+++ loops/loopsProblems/raisepower.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class raisepower {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt();
+        int ans = 1;
+
+        for (int i = 1; i <= b; i++) {
+            ans *= a;
+        }
+        System.out.println(ans);
+    }
+}
