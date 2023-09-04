a661c6f0b85034e203fd180a6396f825bdf5df61
Tribhuwan <tribhuwanja@gmail.com>
2023-09-04 15:19:09 +0530

    pattern lacture is complete

---
+++ NumaricalPattern.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class NumaricalPattern {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int r = sc.nextInt();
+
+        for (int i = 1; i <= r; i++) {
+            for (int j = i; j <= r; j++) {
+                System.out.print(j);
+            }
+
+            for (int k = 1; k <= i-1; k++) {
+                System.out.print(k);
+            }
+
+            System.out.println();
+        }
+    }
+}
--- loops/starPattern.java
+++ loops/starPattern.java
@@ -59,15 +59,40 @@
 // }
 
 // reverse triangular pattern 
+// import java.util.Scanner;
+// public class starPattern {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int r = sc.nextInt();
+//         for(int i=1; i<=r; i++){
+//             for(int j=1; j<= (r+1-i); j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+
+//     }
+// }
+
+
+// Pyramid pattern
 import java.util.Scanner;
 public class starPattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int r = sc.nextInt();
-        for(int i=1; i<=r; i++){
-            for(int j=1; j<= (r+1-i); j++){
+        for (int i = 1; i <= r; i++){
+            //print r-1 spaces
+                for(int j= 1; j<= r-i; j++){
+                    System.out.print(" ");
+                }
+
+            //print 2*i-1 star 
+
+            for(int k=1; k<= 2*i-1; k++){
                 System.out.print("*");
             }
+
             System.out.println();
         }
 
