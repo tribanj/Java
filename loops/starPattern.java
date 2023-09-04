868e5ce1589a3faa73ffc9b1bd2ee0389114e8d7
Tribhuwan <tribhuwanja@gmail.com>
2023-09-04 14:38:44 +0530

    triangle pattern is done

--- loops/starPattern.java
+++ loops/starPattern.java
@@ -19,25 +19,59 @@
 
 // Rectangular pattern border/
 
+// import java.util.Scanner;
+
+// public class starPattern {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int r = sc.nextInt();
+//         int c = sc.nextInt();
+
+//         for(int i = 1; i<=r; i++){
+//             for(int j=1; j<=c; j++){
+//                 if(i==1 || i==r || j==1 || j==c)
+//                 System.out.print("*");
+//                 else
+//                 System.out.print(" ");
+//             }
+//             System.out.println();
+//         }
+
+//     }
+
+// }
+
+// triangular pattern 
+// import java.util.Scanner;
+// public class starPattern {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int r = sc.nextInt();
+//         for(int i=1; i<=r; i++){
+//             for(int j=1; j<=i; j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+
+//     }
+// }
+
+// reverse triangular pattern 
 import java.util.Scanner;
-
 public class starPattern {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int r = sc.nextInt();
-        int c = sc.nextInt();
-
-        for(int i = 1; i<=r; i++){
-            for(int j=1; j<=c; j++){
-                if(i==1 || i==r || j==1 || j==c)
+        for(int i=1; i<=r; i++){
+            for(int j=1; j<= (r+1-i); j++){
                 System.out.print("*");
-                else
-                System.out.print(" ");
             }
             System.out.println();
         }
 
     }
-
 }
+
+
